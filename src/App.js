0be04ca6a361b2fa75d5e27bdab857c9a37fe073
alinabeglarian <alinabeglarian@gmail.com>
2019-06-22 10:54:41 +0200

    step 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+const data = [
+  {
+    name: "Ivel Z3",
+    manufacturer: "Ivasim",
+    year: 1969,
+    origin: "Croatia"
+  },
+  {
+    name: "Bally Astrocade",
+    manufacturer: "Bally Consumer Products",
+    year: 1977,
+    origin: "USA"
+  },
+  {
+    name: "Sord M200 Smart Home Computer",
+    manufacturer: "Sord Computer Corporation",
+    year: 1971,
+    origin: "Japan"
+  },
+  {
+    name: "Commodore 64",
+    manufacturer: "Commodore",
+    year: 1982,
+    origin: "USA"
+  }
+]
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <select>
+          <option value="">-- Pick a model --</option>
+          {data.map((data) => 
+          <option key={data.name} value={data.name}>{data.name} ({data.year})</option>)}
+        </select>
+      </div>
+    )
+  }
 }
 
-export default App;
+
