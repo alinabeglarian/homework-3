16633e4ceecec32cd1f6eb3b1323b8c11662dece
alinabeglarian <alinabeglarian@gmail.com>
2019-06-22 12:11:55 +0200

    step 2

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
 import React from 'react';
 import './App.css';
 
-const data = [
-  {
-    name: "Ivel Z3",
-    manufacturer: "Ivasim",
-    year: 1969,
-    origin: "Croatia"
-  },
-  {
-    name: "Bally Astrocade",
-    manufacturer: "Bally Consumer Products",
-    year: 1977,
-    origin: "USA"
-  },
-  {
-    name: "Sord M200 Smart Home Computer",
-    manufacturer: "Sord Computer Corporation",
-    year: 1971,
-    origin: "Japan"
-  },
-  {
-    name: "Commodore 64",
-    manufacturer: "Commodore",
-    year: 1982,
-    origin: "USA"
-  }
-]
+
 
 export default class App extends React.Component {
+  state = {
+    data: [
+      {
+        name: "Ivel Z3",
+        manufacturer: "Ivasim",
+        year: 1969,
+        origin: "Croatia"
+      },
+      {
+        name: "Bally Astrocade",
+        manufacturer: "Bally Consumer Products",
+        year: 1977,
+        origin: "USA"
+      },
+      {
+        name: "Sord M200 Smart Home Computer",
+        manufacturer: "Sord Computer Corporation",
+        year: 1971,
+        origin: "Japan"
+      },
+      {
+        name: "Commodore 64",
+        manufacturer: "Commodore",
+        year: 1982,
+        origin: "USA"
+      }
+    ]
+  }
+
+  updateSelection = (event) => {
+    this.setState({ value: event.target.value });
+  }
+
   render() {
     return (
       <div className="App">
-        <select>
+        <select value={this.state.value} onChange={this.updateSelection}>
           <option value="">-- Pick a model --</option>
-          {data.map((data) => 
+          {this.state.data.map((data) => 
           <option key={data.name} value={data.name}>{data.name} ({data.year})</option>)}
         </select>
       </div>
